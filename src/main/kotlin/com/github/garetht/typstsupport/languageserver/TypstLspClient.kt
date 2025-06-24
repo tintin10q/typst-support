e749bba4f6ef9fb64e2caef846cb3d909e7d2271
Gareth Tan <garetht@users.noreply.github.com>
2025-06-24 13:11:58 -0700

    Remove unused.

--- src/main/kotlin/com/github/garetht/typstsupport/languageserver/TypstLspClient.kt
+++ src/main/kotlin/com/github/garetht/typstsupport/languageserver/TypstLspClient.kt
@@ -1,19 +1,16 @@
 package com.github.garetht.typstsupport.languageserver
 
 import com.github.garetht.typstsupport.languageserver.models.Outline
-import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.intellij.platform.lsp.api.Lsp4jClient
 import com.intellij.platform.lsp.api.LspServerNotificationsHandler
 import org.eclipse.lsp4j.jsonrpc.services.JsonNotification
 
-private val LOG = logger<TypstLspClient>()
 
 class TypstLspClient(
   project: Project,
   serverNotificationsHandler: LspServerNotificationsHandler,
 ) : Lsp4jClient(serverNotificationsHandler) {
   @JsonNotification("tinymist/documentOutline")
-  fun handleDocumentOutline(_: Outline) {
-  }
+  fun handleDocumentOutline(outline: Outline) = Unit
 }
