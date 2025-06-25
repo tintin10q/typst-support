29a42b3c4c59cbe2ffa2c6f3df44586b0c0f3ec6
Gareth Tan <garetht@users.noreply.github.com>
2025-06-24 19:22:31 -0700

    Remove unsupported file extensions.

--- src/main/kotlin/com/github/garetht/typstsupport/languageserver/locations/TypstSupport.kt
+++ src/main/kotlin/com/github/garetht/typstsupport/languageserver/locations/TypstSupport.kt
@@ -2,7 +2,7 @@ package com.github.garetht.typstsupport.languageserver.locations
 
 import com.intellij.openapi.vfs.VirtualFile
 
-private val supportedTypstExtensions = setOf("typ", "typm", "typc")
+private val supportedTypstExtensions = setOf("typ")
 
 fun VirtualFile.isSupportedTypstFileType(): Boolean {
   return supportedTypstExtensions.contains(extension)
